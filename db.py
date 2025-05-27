6addd91b4cd8f92f69647ab5feac26f3f4ccb07e
Denxhinjo <83427579+Denxhinjo@users.noreply.github.com>
2025-05-27 17:56:18 +0200

    Update db.py

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from supabase import create_client, Client
 
-SUPABASE_URL = "https://watlewvfnqltujhkzfzd.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndhdGxld3ZmbnFsdHVqaGt6ZnpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgyNTg2NTEsImV4cCI6MjA2MzgzNDY1MX0.j1MmQ2xJTkUEnW1dMX0r17BiI3OuiPvc0Tyyr1uPsQg"
+SUPABASE_URL = "supabase_url"
+SUPABASE_KEY = "supabase_key"
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
